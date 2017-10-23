af834d0ef065bdf881677ce2c35f4111a40b58cc
super zozo <Anh.Nguyen3@metropolia.fi>
2017-10-24 01:47:18 +0300

    remove unneccesary code

--- controllers/landing.go
+++ controllers/landing.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/anhnguyen300795/Task-manager/utils"
 )
 
 func (controller *Controllers) LandingController(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		claims := utils.ExtractContext(r)
-		fmt.Println(claims)
 		data, err := ioutil.ReadFile("public/index.html")
 		if err != nil {
 			panic(err)
